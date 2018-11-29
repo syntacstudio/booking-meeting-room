e58878b79bf096bb7359d1ced7147f24c178cb03
Willy Arisky <willy.arisky@gmail.com>
2018-11-29 21:56:13 +0700

    view for admin booking

---
+++ resources/views/admin/booking/detail.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.admin')
+@section('page.title', 'Booking Detail')
+
+@section('content')
+<div class="container">
+    <h5 class="mb-3">Booking Detail</h5>
+</div>
+@endsection
---
+++ resources/views/admin/booking/list.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.admin')
+@section('page.title', 'Bookings List')
+
+@section('content')
+<div class="container">
+    <h5 class="mb-3">Bookings List:</h5>
+</div>
+@endsection
