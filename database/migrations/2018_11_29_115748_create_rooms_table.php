1afbf7b1d65f7d962fb78eff1f704e097778e906
Willy Arisky <willy.arisky@gmail.com>
2018-11-29 21:54:28 +0700

    add migration table rooms

---
+++ database/migrations/2018_11_29_115748_create_rooms_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('location');
+            $table->integer('price');
+            $table->text('description');
+            $table->string('image');
+            $table->text('permalink');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
