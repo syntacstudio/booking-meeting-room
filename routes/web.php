5d8aa67fb2d7bb07ec416b500b0b2d6dabb44327
Willy Arisky <willy.arisky@gmail.com>
2018-11-29 21:53:47 +0700

    Add routes admin

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,13 @@ Auth::routes();
 // routes for admin
 Route::middleware(['auth', 'roles:admin'])->group(function () {
     Route::get('/admin', 'AdminController@index')->name('admin');
+    Route::get('/admin/bookings', 'Admin\BookingController@index')->name('admin.bookings');
+    Route::get('/admin/booking/{$id}', 'Admin\BookingController@detail')->name('admin.booking');
+
+    Route::get('/admin/rooms', 'Admin\RoomController@index')->name('admin.rooms');
+    Route::get('/admin/room/{$id}', 'Admin\RoomController@detail')->name('admin.room');
+    Route::get('/admin/room/new', 'Admin\RoomController@new')->name('admin.room.new');
+    Route::post('/admin/room/store', 'Admin\RoomController@store')->name('admin.room.store');
 });
 
 // routes for user
