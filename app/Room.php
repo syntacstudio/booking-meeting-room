251f0d75d6d7f1721ab25ae399b34caee6cf58c2
Willy Arisky <willy.arisky@gmail.com>
2018-11-29 21:54:49 +0700

    add model Room, add generate permalink function

---
+++ app/Room.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Room;
+
+class Room extends Model
+{
+    /**
+     * Empty guarded fields
+     */
+    protected $guarded = [];
+
+    /**
+     * @param $name
+     * @param int $id
+     * @return string
+     * @throws \Exception
+     */
+    public static function createPermalink($name, $id = 0)
+    {
+        // Normalize the name
+        $slug = str_slug($name);
+        // Get any that could possibly be related.
+        // This cuts the queries down by doing it once.
+        $allSlugs = self::getRelatedPermalink($slug, $id);
+        // If we haven't used it before then we are all good.
+        if (! $allSlugs->contains('slug', $slug)){
+            return $slug;
+        }
+        // Just append numbers like a savage until we find not used.
+        for ($i = 1; $i <= 10; $i++) {
+            $newSlug = $slug.'-'.$i;
+            if (! $allSlugs->contains('slug', $newSlug)) {
+                return $newSlug;
+            }
+        }
+        throw new \Exception('Can not create a unique permalink');
+    }
+
+    protected static function getRelatedPermalink($slug, $id = 0)
+    {
+        return self::select('permalink')->where('permalink', 'like', $slug.'%')
+            ->where('id', '<>', $id)
+            ->get();
+    }
+}
