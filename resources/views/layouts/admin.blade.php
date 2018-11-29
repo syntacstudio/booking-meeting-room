de94de1aea8da854763f89fa2185d8d3d3eb534d
Willy Arisky <willy.arisky@gmail.com>
2018-11-29 21:53:28 +0700

    Major changes

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,44 +4,50 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('page.title')</title>
+    <title>@yield('page.title') • Administrator</title>
     <script src="{{ asset('js/app.js') }}" defer></script>
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+        <nav class="navbar navbar-expand-md navbar-light">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link {{ active_nav('admin') }}" href="{{ route('admin') }}">DASHBOARD</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{ active_nav('admin.bookings') }}" href="{{ route('admin.bookings') }}">BOOKINGS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{ active_nav('admin.rooms') }}" href="{{ route('admin.rooms') }}">MANAGE ROOM</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{ active_nav('home') }}" href="{{ route('admin') }}">CUSTOMERS</a>
+                        </li>
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
-                        <li class="nav-item">
-                            <a class="nav-link {{ active_nav('home') }}" href="{{ route('home') }}">HOME</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link {{ active_nav('home') }}" href="{{ route('home') }}">EXPLORE</a>
-                        </li>
+                        <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">Login</a>
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                             <li class="nav-item">
                                 @if (Route::has('register'))
-                                    <a class="nav-link" href="{{ route('register') }}">Register</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 @endif
                             </li>
                         @else
                             <li class="nav-item dropdown">
-                            @if (Auth::user()->hasRole('admin'))
-                                <a class="btn btn-primary ml-md-3" href="{{ route('admin') }}"><i class="fa fa-arrow-left"></i> Back to Admin</a>
-                            @else
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
@@ -50,13 +56,13 @@
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        Logout
+                                        {{ __('Logout') }}
                                     </a>
+
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
                                 </div>
-                            @endif
                             </li>
                         @endguest
                     </ul>
@@ -64,7 +70,7 @@
             </div>
         </nav>
 
-        <main>
+        <main class="py-4">
             @yield('content')
         </main>
     </div>
