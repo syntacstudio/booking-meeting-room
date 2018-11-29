f7bb61462b73ed77cc713e485c7b8801e159345f
Willy Arisky <willy.arisky@gmail.com>
2018-11-29 21:55:19 +0700

    Add count rooms data to view admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Room;
 
 class AdminController extends Controller
 {
@@ -11,6 +12,8 @@ class AdminController extends Controller
      */
     public function index()
     {
-        return view('admin');
+    	$rooms = Room::get()->count();
+
+        return view('admin', compact('rooms'));
     }
 }
