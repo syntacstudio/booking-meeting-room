990eebf6eeef5562b0bff1d5a829e79578b42e5f
Willy Arisky <willy.arisky@gmail.com>
2018-11-30 13:53:43 +0700

    change layout and add padding

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.default')
 
 @section('content')
-<div class="container">
+<div class="container py-5">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
