3f8ab6dacff0d35a8b38b880aba0eecb23768cc2
Willy Arisky <willy.arisky@gmail.com>
2018-11-30 23:32:37 +0700

    add booking data

--- resources/views/admin/booking/list.blade.php
+++ resources/views/admin/booking/list.blade.php
@@ -4,5 +4,37 @@
 @section('content')
 <div class="container">
     <h5 class="mb-3">Bookings List:</h5>
+
+    <table class="table table-hover table-bordered">
+    	<thead class="thead-light">
+    		<tr>
+    			<th>#</th>
+    			<th>Customer</th>
+                <th>Meeting Date</th>
+                <th>Day</th>
+    			<th>Booked at</th>
+    			<th>Meeting Room</th>
+    			<th>Total</th>
+    			<th></th>
+    		</tr>
+    	</thead>
+    	<body>
+    		@foreach($data as $key => $booking)
+    		<tr>
+    			<th>{{ $key+1 }}</th>
+    			<td>{{ $booking->customer->name }}</td>
+                <td>{{ $booking->start_date->format('Y/m/d') }}</td>
+                <td>{{ $booking->day }}</td>
+    			<td>{{ $booking->created_at->format('Y/m/d') }}</td>
+    			<td>{{ $booking->room->name }}</td>
+    			<td>${{ $booking->total }}</td>
+    			<td class="text-right">
+    				
+    			</td>
+    		</tr>
+    		@endforeach
+    	</body>
+    </table>
+
 </div>
 @endsection
