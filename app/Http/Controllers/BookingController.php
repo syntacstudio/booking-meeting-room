f01f7a073efac701509b7b15371aaa25dba245e6
Willy Arisky <willy.arisky@gmail.com>
2018-11-30 22:07:20 +0700

    add store booking

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-use Auth;
+use Carbon\Carbon;
+use Stripe\Stripe;
+use Stripe\Charge;
 use App\Room;
+use App\Booking;
+use Auth;
 
 class BookingController extends Controller
 {
@@ -44,6 +48,65 @@ class BookingController extends Controller
             	'room' => $room, 
             	'email' => $email 
             ];
+        }
+
+    	return response()->json($response);
+    }
+
+    /**
+     * Store the booking data
+     */
+    public function store(Request $request)
+    {	
+    	$user = Auth::user();
+    	$room = Room::find($request->room);
+
+    	try {
+    		Stripe::setApiKey(env('STRIPE_SECRET'));
+
+    		Charge::create([
+			    'amount' => ($room->price * 100) * $request->day,
+			    'currency' => 'usd',
+			    'description' => 'Booking: '.$room->name,
+			    'source' => $request->stripeToken,
+			    'receipt_email' => $user->email,
+			]);
+
+			$booking = Booking::create([
+				'number' => 'ID'.date('ymdGis'),
+				'user_id' => $user->id,
+				'room_id' => $room->id,
+				'start_date' => Carbon::parse($request->date),
+				'end_date' => Carbon::parse($request->date)->addDays($request->day),
+				'day' =>$request->day,
+				'total' => $room->price * $request->day,
+				'note' => $request->note
+			]);
+
+    		$response = [ 'status' => 'success', 'msg' => 'Thank you! Your payment has been successfully received.', 'data' => $booking ];
+    	} catch(\Stripe\Error\Card $e) {
+            $body = $e->getJsonBody();
+            $errors[] = $body['error']['message'];
+        } catch (\Stripe\Error\RateLimit $e) {
+            $body = $e->getJsonBody();
+            $errors[] = $body['error']['message'];
+        } catch (\Stripe\Error\InvalidRequest $e) {
+            $body = $e->getJsonBody();
+            $errors[] = $body['error']['message'];
+        } catch (\Stripe\Error\Authentication $e) {
+            $body = $e->getJsonBody();
+            $errors[] = $body['error']['message'];
+        } catch (\Stripe\Error\ApiConnection $e) {
+            $body = $e->getJsonBody();
+            $errors[] = $body['error']['message'];
+        } catch (\Stripe\Error\Base $e) {
+            $body = $e->getJsonBody();
+            $errors[] = $body['error']['message'];
+        } catch (Exception $e) {
+            $body = $e->getJsonBody();
+            $errors[] = $body['error']['message'];
+        } catch(Exception $e) {
+            $response = ['status' => 'Failed'];
         }
 
     	return response()->json($response);
