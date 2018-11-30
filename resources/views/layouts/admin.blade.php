2dc8669d9bd643565e0497af8598447f1c212c5b
Willy Arisky <willy.arisky@gmail.com>
2018-11-30 13:44:39 +0700

    Change menu

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -47,22 +47,15 @@
                                 @endif
                             </li>
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                    Logout <i class="fa fa-sign-out"></i>
                                 </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
+                                <form id="logout-form" method="post" class="d-none" action="{{ route('logout') }}">
+                                    @csrf
+                                </form>
                             </li>
                         @endguest
                     </ul>
