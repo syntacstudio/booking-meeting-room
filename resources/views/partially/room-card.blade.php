1b130b63aceceec62bfb8de8fd3ddf548c133bf7
Willy Arisky <willy.arisky@gmail.com>
2018-11-30 13:24:35 +0700

    Change url booking, vuew

--- resources/views/partially/room-card.blade.php
+++ resources/views/partially/room-card.blade.php
@@ -1,15 +1,14 @@
 			<div class="card mb-4 box-shadow">
                 <img class="card-img-top" src="{{ asset('storage/rooms/'. $room->image) }}">
                 <div class="card-body">
-                	<h1 class="h5">{{ $room->name }}</h1>
+                	<h1 class="h5">
+                		<a href="{{ route('room', $room->permalink) }}">{{ $room->name }}</a>
+                	</h1>
                 	<p class="text-muted">{{ $room->location }}</p>
 	                <p class="card-text">{{ str_limit($room->description, 135, ' ...') }}</p>
 	                <div class="d-flex justify-content-between align-items-center">
-	                	<span class="h4">${{ $room->price }} / Hour</span>
-	                    <div class="btn-group">
-	                      <a href="/room/{{ $room->permalink }}" class="btn btn-sm btn-outline-secondary">View</a>
-	                      <a href="/booking/{{ $room->permalink }}" class="btn btn-sm btn-outline-secondary">Book</a>
-	                    </div>
+	                	<span class="h4">${{ $room->price }} <span class="text-muted">/</span> Hour</span>
+	                    <a href="{{ route('booking', $room->permalink) }}" class="btn btn-sm btn-outline-primary">Book Now!</a>
 	                </div>
                 </div>
             </div>
