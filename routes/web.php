01718f2491dbd33368ba445edc017b7db8efe6ad
Willy Arisky <willy.arisky@gmail.com>
2018-11-30 09:35:21 +0700

    add route edit and update room

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,11 @@ Route::middleware(['auth', 'roles:admin'])->group(function () {
     Route::get('/admin/booking/{$id}', 'Admin\BookingController@detail')->name('admin.booking');
 
     Route::get('/admin/rooms', 'Admin\RoomController@index')->name('admin.rooms');
-    Route::get('/admin/room/{$id}', 'Admin\RoomController@detail')->name('admin.room');
+    Route::get('/admin/room/{id}', 'Admin\RoomController@detail')->name('admin.room');
     Route::get('/admin/room/new', 'Admin\RoomController@new')->name('admin.room.new');
     Route::post('/admin/room/store', 'Admin\RoomController@store')->name('admin.room.store');
+    Route::get('/admin/room/{id}/edit/', 'Admin\RoomController@edit')->name('admin.room.edit');
+    Route::post('/admin/room/{id}/update', 'Admin\RoomController@update')->name('admin.room.update');
 });
 
 // routes for user
