03552c8be3246e8532b20e6e124df4bfd758c88b
Willy Arisky <willy.arisky@gmail.com>
2018-11-30 23:33:14 +0700

    change design booking

--- resources/views/booking.blade.php
+++ resources/views/booking.blade.php
@@ -2,9 +2,12 @@
 @section('page.title', 'Booking: '. $data->name)
 
 @section('content')
+@include('partially.loading')
 <div id="booking-room">
     <div class="container py-5 mb-md-5">
-    	<div class="row mb-md-5">
+      <form id="booking-form" data-room="{{ $data->permalink }}">
+        @csrf
+    	<div class="row">
             <div class="col-md-8">
                 <p class="h4 mb-3">Booking meeting room:</p>
                 <div class="card p-3">
@@ -29,96 +32,33 @@
 
                         <div class="form-group">
                             <label for="name">Your Name</label>
-                            <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') ?? $customer->name }}" placeholder="Your Full Name">
-                            @if ($errors->has('name'))
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $errors->first('name') }}</strong>
-                                </span>
-                            @endif
+                            <input id="name" type="text" class="form-control" name="name" value="{{ $customer->name }}" placeholder="Your Full Name">
+                            <span class="invalid-feedback" id="name-feedback">
+                                <strong></strong>
+                            </span>
                         </div>
 
                         <div class="row">
-                            <div class="form-group col-md-6">
+                            <div class="form-group col-md-4">
                                 <label for="date">Date of Meeting</label>
-                                <input id="date" type="text" class="form-control{{ $errors->has('date') ? ' is-invalid' : '' }}" name="date" value="{{ old('date') }}" placeholder="Date">
-                                @if ($errors->has('date'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('date') }}</strong>
-                                    </span>
-                                @endif
+                                <input id="date" type="date" min='{{ date('Y-m-d') }}' class="form-control" name="date" value="{{ date('Y-m-d') }}" placeholder="Date of Meeting">
+                                <span class="invalid-feedback" id="date-feedback">
+                                    <strong></strong>
+                                </span>
                             </div>
-                            <div class="form-group col-md-3">
-                                <label for="date">Start at</label>
-                                <select name="start_at" class="custom-select {{ $errors->has('start_at') ? ' is-invalid' : '' }}">
-                                  <option selected>Start at</option>
-                                  <option value="01">01:00</option>
-                                  <option value="02">02:00</option>
-                                  <option value="03">03:00</option>
-                                  <option value="04">04:00</option>
-                                  <option value="05">05:00</option>
-                                  <option value="06">06:00</option>
-                                  <option value="07">07:00</option>
-                                  <option value="08">08:00</option>
-                                  <option value="09">09:00</option>
-                                  <option value="10">10:00</option>
-                                  <option value="11">11:00</option>
-                                  <option value="12">12:00</option>
-                                  <option value="13">13:00</option>
-                                  <option value="14">14:00</option>
-                                  <option value="15">15:00</option>
-                                  <option value="16">16:00</option>
-                                  <option value="17">17:00</option>
-                                  <option value="18">18:00</option>
-                                  <option value="19">19:00</option>
-                                  <option value="20">20:00</option>
-                                  <option value="21">21:00</option>
-                                  <option value="22">22:00</option>
-                                  <option value="23">23:00</option>
-                                  <option value="24">24:00</option>
-                                </select>
-                                @if ($errors->has('start_at'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('start_at') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                            <div class="form-group col-md-3">
-                                <label for="date">End at</label>
-                                <select name="end_at" class="custom-select {{ $errors->has('end_at') ? ' is-invalid' : '' }}">
-                                  <option selected>End at</option>
-                                  <option value="01">01:00</option>
-                                  <option value="02">02:00</option>
-                                  <option value="03">03:00</option>
-                                  <option value="04">04:00</option>
-                                  <option value="05">05:00</option>
-                                  <option value="06">06:00</option>
-                                  <option value="07">07:00</option>
-                                  <option value="08">08:00</option>
-                                  <option value="09">09:00</option>
-                                  <option value="10">10:00</option>
-                                  <option value="11">11:00</option>
-                                  <option value="12">12:00</option>
-                                  <option value="13">13:00</option>
-                                  <option value="14">14:00</option>
-                                  <option value="15">15:00</option>
-                                  <option value="16">16:00</option>
-                                  <option value="17">17:00</option>
-                                  <option value="18">18:00</option>
-                                  <option value="19">19:00</option>
-                                  <option value="20">20:00</option>
-                                  <option value="21">21:00</option>
-                                  <option value="22">22:00</option>
-                                  <option value="23">23:00</option>
-                                  <option value="24">24:00</option>
-                                </select>
-                                @if ($errors->has('end_at'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('end_at') }}</strong>
-                                    </span>
-                                @endif
+                            <div class="form-group col-md-4">
+                                <label for="day">Number of Day</label>
+                                <input id="day" type="number" min="1" class="form-control" name="day" value="1" placeholder="Date">
+                               <span class="invalid-feedback" id="day-feedback">
+                                    <strong></strong>
+                                </span>
                             </div>
                         </div>
 
+                        <div class="form-group">
+                            <label for="description">Note</label>
+                            <textarea name="note" rows="3" id="note" class="form-control" placeholder="Your note. (Optional)">{{ old('note') }}</textarea>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -127,27 +67,33 @@
                 <div class="card">
                     <div class="card-body">
                         <div class="d-flex justify-content-between align-items-center pb-2">
-                            <span>Price per hour</span>
-                            <b>$<span id="price">{{ $data->price }}</span></b>
+                            <span>Price per day</span>
+                            <b>$<span id="price" data-price="{{ $data->price }}">{{ $data->price }}</span></b>
                         </div>
                         <div class="d-flex justify-content-between align-items-center border-bottom pb-2 mt-2">
-                            <span>Number of hours</span>
-                            <span id="num-hours" class="font-weight-bold">-</span>
+                            <span>Number of days</span>
+                            <span><span id="num-days" class="font-weight-bold">1</span> day</span>
                         </div>
                         <div class="d-flex justify-content-between align-items-center mt-2">
-                            <span class="h4 mb-0">Total</span>
-                            <span id="num-hours" class="h4 mb-0">
+                            <span class="h5 mb-0 font-weight-bold">Total</span>
+                            <span class="h5 mb-0 font-weight-bold">
                                 $<span id="total">{{ $data->price }}</span>
                             </span>
                         </div>
                     </div>
                 </div>
 
-                <div class="form-group mt-2">
-                    <button class="btn btn-lg btn-block btn-primary">PAY NOW</button>
+                <div class="form-group mt-3 row">
+                    <div class="col-md-8">
+                        <img class="w-100 mt-1" src="{{ asset('/images/credit-cards.png') }}">
+                    </div>
+                    <div class="col-md-4">
+                        <button type="submit" class="btn btn-block btn-primary">PAY NOW</button>
+                    </div>
                 </div>
             </div>
         </div>
+      </form>
     </div>
 </div>
 @endsection
