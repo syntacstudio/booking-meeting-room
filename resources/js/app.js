c3d27126e39fb6b305497e3f64330410046f4a22
Willy Arisky <willy.arisky@gmail.com>
2018-11-30 09:35:58 +0700

    add open upload file for edit image room

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+
+// open upload file when edit room
+$('input[name="edit_image"]').on('click', function(){
+    if($(this).is(':checked')) {
+        $('#image_upload').slideDown();
+    } else {
+        $('#image_upload').slideUp();
+    }
+});
