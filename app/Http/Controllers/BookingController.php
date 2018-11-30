ac224330f4d882bc2d894eb904f4a76c3b0ca6bc
Willy Arisky <willy.arisky@gmail.com>
2018-11-30 17:36:36 +0700

    add booking validation field

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Auth;
+use App\Room;
 
 class BookingController extends Controller
 {
@@ -15,5 +18,34 @@ class BookingController extends Controller
         $customer  = Auth::user();
 
     	return view('booking', compact('data', 'customer'));
+    }
+
+    /**
+     * Validation booking data
+     */
+    public function validation(Request $request)
+    {
+    	$validator = Validator::make($request->all(), [
+    		'name' => 'required|string|max:225',
+    		'date' => 'required|string|max:225',
+    		'day' => 'required|integer|min:1',
+    	]);
+
+        if($validator->fails())
+        {
+            $response = [ 'status' => 'error', 'errors' => $validator->errors() ];            
+        } else {
+        	$room 	= Room::where('permalink', $request->permalink)->first();
+        	$email 	= Auth::user()->email;
+
+            $response = [ 
+            	'status' => 'valid', 
+            	'msg' => 'Data validated.', 
+            	'room' => $room, 
+            	'email' => $email 
+            ];
+        }
+
+    	return response()->json($response);
     }
 }
