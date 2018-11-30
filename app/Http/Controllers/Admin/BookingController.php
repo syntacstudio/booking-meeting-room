73a6249e652c5114130ef91fc028014b22e443cb
Willy Arisky <willy.arisky@gmail.com>
2018-11-30 23:33:50 +0700

    add booking data to admin bookings list

--- app/Http/Controllers/Admin/BookingController.php
+++ app/Http/Controllers/Admin/BookingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Booking;
 
 class BookingController extends Controller
 {
@@ -12,7 +13,10 @@ class BookingController extends Controller
      */
     public function index()
     {
-    	return view('admin.booking.list');
+        $data = Booking::orderBy('id', 'DESC')
+                        ->paginate(10);
+
+    	return view('admin.booking.list', compact('data'));
     }
 
     /**
