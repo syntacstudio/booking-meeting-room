3fac4f69f94b259f1d2f1f27db97ace125e9d982
Willy Arisky <willy.arisky@gmail.com>
2018-11-30 13:44:29 +0700

    Add controller for room

---
+++ app/Http/Controllers/RoomController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Room;
+
+class RoomController extends Controller
+{
+    /**
+     * Browse meeting rooms page 
+     */
+    public function browse()
+    {
+    	$rooms = Room::paginate(12);
+
+    	return view('browse', compact('rooms'));
+    }
+
+    /**
+     * Detail meeting room
+     */
+    public function detail($permalink)
+    {
+    	$data = Room::where('permalink', $permalink)->first();
+
+    	return view('room', compact('data'));
+    }
+
+    /**
+     * Booking meeting room
+     */
+    public function booking($permalink)
+    {
+    	$data = Room::where('permalink', $permalink)->first();
+
+    	return view('booking', compact('data'));
+    }
+}
