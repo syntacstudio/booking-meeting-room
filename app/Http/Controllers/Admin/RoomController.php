315340fde66db227dc4cd5668d8ce356f18dded0
Willy Arisky <willy.arisky@gmail.com>
2018-11-30 09:41:54 +0700

    add destroy

--- app/Http/Controllers/Admin/RoomController.php
+++ app/Http/Controllers/Admin/RoomController.php
@@ -63,6 +63,70 @@ class RoomController extends Controller
 
         return redirect()
                     ->route('admin.rooms')
-                    ->with('success', 'Data has been added.');   
+                    ->with('success', 'Meetimg room has been added.');   
+    }
+
+    /**
+     * View edit room
+     */
+    public function edit($id)
+    {
+        $data = Room::find($id);
+
+        return view('admin.room.edit', compact('data'));
+    }
+
+    /**
+     * Update room data
+     */
+    public function update(Request $request, $id)
+    {   
+        $room = Room::find($id);
+
+        $rules = [
+            'name' => 'required|string|max:225',
+            'location' => 'required|string|max:225',
+            'price' => 'required|integer',
+            'description' => 'required|string',
+        ];
+
+        if($request->edit_image) {
+            $rules['image'] = 'required|image|max:1024';
+        }
+
+        $validate = $request->validate($rules);
+
+        $room->name         = $request->name;
+        $room->location     = $request->location;
+        $room->price        = $request->price;
+        $room->description  = $request->description;
+
+        if($request->edit_image) {
+            Storage::delete('/public/rooms' . $room->image);
+            $image      = $request->file('image');
+            $filename   = date('ymdHis').'.'.$request->image->getClientOriginalExtension();
+            $image->storeAs('public/rooms', $filename);
+            $room->image        = $filename;
+        }
+
+        $room->save();
+
+        return redirect()
+                    ->route('admin.rooms')
+                    ->with('success', 'Meetimg room data has been updated.');
+    }
+
+    /**
+     * Destroy data
+     */
+    public function destroy($id)
+    {
+        $room = Room::find($id);
+        Storage::delete('/public/rooms' . $room->image);
+        $room->delete();
+
+        return redirect()
+                    ->route('admin.rooms')
+                    ->with('success', 'Meetimg room has been deleted.');
     }
 }
