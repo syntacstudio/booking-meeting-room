491dad2ffaca174f5d2c59e399d0384bad7f5409
Willy Arisky <willy.arisky@gmail.com>
2018-11-30 23:35:20 +0700

    change redirect destination

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,7 +29,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/browse';
 
     /**
      * Create a new controller instance.
