553ebd9f67bdad2807f584238105bce23a1ad940
Willy Arisky <willy.arisky@gmail.com>
2018-11-30 17:36:22 +0700

    add migration for table bookings

---
+++ database/migrations/2018_11_30_101630_create_bookings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('number');
+            $table->integer('user_id');
+            $table->integer('room_id');
+            $table->string('start_date');
+            $table->string('end_date');
+            $table->integer('day');
+            $table->integer('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
