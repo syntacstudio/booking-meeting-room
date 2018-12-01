205e28bb9cce1126bc12391fdc0f92d0e22303a4
Willy Arisky <willy.arisky@gmail.com>
2018-12-01 21:43:28 +0700

    add push alert

--- resources/js/app.js
+++ resources/js/app.js
@@ -32,6 +32,12 @@ function clearFeedback() {
 	$('.invalid-feedback strong').text('');
 }
 
+//push alert
+function pushAlert( type, msg, wrapper ) {
+    var html = '<div class="alert alert-'+ type +' alert-dismissible fade show alert-action" role="alert"> '+ msg +' <button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span> </button> </div>';
+    $('.'+wrapper).prepend(html);
+}
+
 /**
  * Change total booking
  */
