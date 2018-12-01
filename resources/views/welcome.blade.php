c86e7e373b18c55b604114612a44bdb85585553c
Willy Arisky <willy.arisky@gmail.com>
2018-12-01 23:00:46 +0700

    add filed name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,7 @@
     	<form method="get" action="{{ route('browse') }}" class="row bg-white box-shadow px-3 pb-3 mt-4 pt-4 rounded w-75 mx-auto mt-3">
     		<div class="form-group col-md-9">
     			<input class="form-control form-control-lg" placeholder="Search city, name or anything ...">
+    			<input class="form-control form-control-lg" name="search" placeholder="Search city, name or anything ...">
     		</div>
     		<div class="form-group col-md-3">
     			<button class="btn btn-lg btn-block btn-primary">SEARCH</button>
