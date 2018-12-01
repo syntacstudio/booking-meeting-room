4174c688a194394ebe5d661d2e0e61d73d3582d1
Willy Arisky <willy.arisky@gmail.com>
2018-12-01 21:47:18 +0700

    add function update profile and password

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\Booking;
 use Auth;
 
@@ -39,6 +40,65 @@ class AccountController extends Controller
     	$data = Booking::where('number', $number)->first();
 
     	return view('account.invoice', compact('data'));
+    }
+
+    /**
+     * Update profile
+     */
+    public function update(Request $request)
+    {
+        $user   = Auth::user();
+        $rules  = ['name' => 'required|string|max:225'];
+
+        if($user->email != $request->email || empty($request->email)){
+            $rules['email'] = 'required|unique:users,email';
+        }
+
+        $validator = Validator::make($request->all(),$rules);
+
+        if($validator->fails())
+        {
+            $response = ['status' => 'error', 'errors' => $validator->errors()];            
+        } else {
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $saved = $user->save();
+
+            if($saved){
+                $response = ['status' => 'success', 'msg' => 'Your profile has been update.'];
+            } else {
+                $response = ['status' => 'failed', 'msg' => 'Failed to update your profile.'];
+            }
+        }
+
+        return response()->json($response);
+    }
+
+    /**
+     * Update password
+     */
+    public function updatePassword(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'password' => 'required|min:5'
+        ]);
+
+        if($validator->fails())
+        {
+            $response = ['status' => 'error', 'errors' => $validator->errors()];            
+        } else {
+            $user = Auth::user();
+            $user->password = bcrypt($request->password);
+            $saved = $user->save();
+
+            if($saved){
+                $response = ['status' => 'success', 'msg' => 'Your password has been update.'];
+            } else {
+                $response = ['status' => 'failed', 'msg' => 'Failed to update your password.'];
+            }
+        }
+
+        return response()->json($response);
     }
 
 }
