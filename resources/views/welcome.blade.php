d60689e090e88ec5d356d830b9aa58a0740d4304
Willy Arisky <willy.arisky@gmail.com>
2018-12-01 23:00:59 +0700

    random stuff

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,10 +6,8 @@
     <div class="container">
     	<h1>Find your best meeting room</h1>
 
-    	<form method="get" action="/search" class="row bg-white box-shadow px-3 pb-3 mt-4 pt-4 rounded w-75 mx-auto mt-3">
     	<form method="get" action="{{ route('browse') }}" class="row bg-white box-shadow px-3 pb-3 mt-4 pt-4 rounded w-75 mx-auto mt-3">
     		<div class="form-group col-md-9">
-    			<input class="form-control form-control-lg" placeholder="Search city, name or anything ...">
     			<input class="form-control form-control-lg" name="search" placeholder="Search city, name or anything ...">
     		</div>
     		<div class="form-group col-md-3">
@@ -19,17 +17,21 @@
     </div>
 </div>
 
-<div class="container py-5">
+<div class="container py-5 mb-3">
 	<h2 class="text-center">Featured Meeting Rooms</h2>
 
 	<div id="rooms-listing" class="row mt-5">
-		@foreach($rooms as $room)
-		<div class="col-md-4">
-			@include('partially.room-card')
-		</div>
-		@endforeach
-
-        {{ $rooms->links() }}
+        @if($rooms)
+            @foreach($rooms as $room)
+            <div class="col-md-4">
+                @include('partially.room-card')
+            </div>
+            @endforeach
+        @else
+            <div class="w-100 mx-3 alert alert-info">Not yet data available.</div>
+        @endif
 	</div>
+
+    <a href="{{ route('browse') }}" class="btn float-right btn-secondary">Browse All <i class="fa fa-angle-right"></i></a>
 </div>
 @endsection
