607d3beb18ad58fcea9e1e6f432014c4b7eeca7b
Willy Arisky <willy.arisky@gmail.com>
2018-12-01 17:32:40 +0700

    Compile update assets

--- public/js/app.js
+++ public/js/app.js
@@ -13928,13 +13928,107 @@ module.exports = __webpack_require__(43);
 
 __webpack_require__(13);
 
+// enable tooltip everywhere
+$(function () {
+	$('[data-toggle="tooltip"]').tooltip();
+});
+
+// stripe key
+var stripeKey = $('meta[name=stripe]').attr('content');
+
 // open upload file when edit room
 $('input[name="edit_image"]').on('click', function () {
-    if ($(this).is(':checked')) {
-        $('#image_upload').slideDown();
-    } else {
-        $('#image_upload').slideUp();
-    }
+	if ($(this).is(':checked')) {
+		$('#image_upload').slideDown();
+	} else {
+		$('#image_upload').slideUp();
+	}
+});
+
+// push field feedback
+function pushFeedback(field, msg) {
+	$('#' + field).addClass('is-invalid');
+	$('#' + field + '-feedback').text(msg);
+}
+
+// clear field feedback
+function clearFeedback() {
+	$('.form-control').removeClass('is-invalid');
+	$('.invalid-feedback strong').text('');
+}
+
+/**
+ * Change total booking
+ */
+$('#booking-room #day').change(function (e) {
+	var day = $(this).val();
+	var price = $('#price').data('price');
+	$('#num-days').text(day);
+	$('#total').text(parseInt(price) * parseInt(day));
+});
+
+/**
+ * Checkout booking
+ */
+var booking_form = $('#booking-form');
+
+booking_form.submit(function (e) {
+	e.preventDefault();
+
+	clearFeedback();
+	$('.loader').fadeIn();
+	$.ajax({
+		url: '/booking/validation',
+		type: 'POST',
+		dataType: 'JSON',
+		data: booking_form.serialize() + '&permalink=' + booking_form.data('room')
+	}).done(function ($data) {
+		if ($data.errors) {
+			$.each($data.errors, function (field, msg) {
+				pushFeedback(field, msg);
+			});
+		} else if ($data.status == 'valid') {
+
+			// stripe instance
+			var checkout = StripeCheckout.configure({
+				key: stripeKey,
+				locale: 'auto',
+				token: function token(_token) {
+					$('.loader').fadeIn();
+					$.ajax({
+						url: '/booking/store',
+						type: 'POST',
+						dataType: 'JSON',
+						data: booking_form.serialize() + '&room=' + $data.room.id + '&stripeToken=' + _token.id
+					}).done(function ($data) {
+						console.log($data);
+					}).fail(function ($data) {
+						console.log($data);
+					}).always(function () {
+						$('.loader').fadeOut();
+					});
+				}
+			});
+
+			// open instance
+			checkout.open({
+				name: 'BizRoom',
+				description: 'Booking: ' + $data.room.name,
+				email: $data.email,
+				amount: $data.room.price * $('#booking-room #day').val() * 100
+			});
+			$('.loader').fadeOut();
+		}
+	}).fail(function ($data) {
+		$('.loader').fadeOut();
+		console.log($data);
+	});
+});
+
+$('#print-invoice button').click(function (e) {
+	e.preventDefault();
+	$(this).hide();
+	window.print();
 });
 
 /***/ }),
