1a7fbbb9d536abb99a10af0a63b22405ffa4e121
Willy Arisky <willy.arisky@gmail.com>
2018-12-01 23:00:24 +0700

    change action url with route

--- resources/views/browse.blade.php
+++ resources/views/browse.blade.php
@@ -1,18 +1,54 @@
 @extends('layouts.default')
-@section('page.title', 'Home')
+@section('page.title', 'Browse Meeting Rooms')
 
 @section('content')
-<div class="container py-5">
-	<h2 class="text-center">Meeting Rooms</h2>
-
-	<div id="rooms-listing" class="row mt-5">
-		@foreach($rooms as $room)
-		<div class="col-md-4">
-			@include('partially.room-card')
+<div class="container pb-5 pt-4">
+	<div class="filters border-bottom pb-2">
+		<div class="row">
+			<div class="col-md-8">
+				@if (!$search)
+					<h1 class="h4 pt-2">Meeting Rooms</h1>
+				@else
+					<div class="pt-2">
+						<span class="h4">Search result for: {{ $search }} </span>
+						<small><a href="{{ route('browse') }}" class="float-right mt-2">Clear search</a></small>
+					</div>
+				@endif
+			</div>
+			<div class="col-md-4">
+			  <form method="get">
+				<div class="input-group">
+				  	<input type="text" name="search" class="form-control" value="{{ $search }}" placeholder="Search...">
+					  <div class="input-group-append">
+					    <button class="btn btn-primary" type="button"><i class="fa fa-search"></i></button>
+					  </div>
+				</div>
+			  </form>
+			</div>
 		</div>
-		@endforeach
+	</div>
 
-		{{ $rooms->links() }}
+	<div id="rooms-listing" class="row mt-4">
+		@if($rooms->count() > 0)
+            @foreach($rooms as $room)
+            <div class="col-md-4">
+                @include('partially.room-card')
+            </div>
+            @endforeach
+        @else
+            @if($search)
+            	<div class="w-100 mx-3 alert alert-info">Your search did not match any meeting room.</div>
+        	@else
+        		<div class="w-100 mx-3 alert alert-info">Not yet data available.</div>
+        	@endif
+        @endif
+
+	</div>
+
+	<div class="paginate-wrapper mb-3">
+		<span class="float-right">
+			{{ $rooms->links() }}
+		</span>
 	</div>
 </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
     	<h1>Find your best meeting room</h1>
 
     	<form method="get" action="/search" class="row bg-white box-shadow px-3 pb-3 mt-4 pt-4 rounded w-75 mx-auto mt-3">
+    	<form method="get" action="{{ route('browse') }}" class="row bg-white box-shadow px-3 pb-3 mt-4 pt-4 rounded w-75 mx-auto mt-3">
     		<div class="form-group col-md-9">
     			<input class="form-control form-control-lg" placeholder="Search city, name or anything ...">
     		</div>
