affb0eedb77ec7e278862f3ff7427f6d9a0bc52e
Willy Arisky <willy.arisky@gmail.com>
2018-12-01 14:12:01 +0700

    add partial nav for account

---
+++ resources/views/partially/nav/account.blade.php
@@ -0,0 +1,15 @@
+            <h4>My Account</h4>
+            <ul class="nav flex-md-column mb-md-0 mb-2">
+              <li class="nav-item">
+                <a class="nav-link {{ active_nav('account') }}" href="{{ route('account') }}">BOOKINGS</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link {{ active_nav('account.settings') }}" href="{{ route('account.settings') }}">SETTINGS</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{ route('logout') }}"
+                   onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                    LOGOUT
+                </a>
+              </li>
+            </ul>
