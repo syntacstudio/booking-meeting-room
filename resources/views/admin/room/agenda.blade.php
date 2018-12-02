14e8c0d222bbfb25983cf6a98af40b3511ab8fd4
Willy Arisky <willy.arisky@gmail.com>
2018-12-03 06:04:20 +0700

    add agenda view

---
+++ resources/views/admin/room/agenda.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.admin')
+@section('page.title', 'Agenda meeting room')
+
+@section('content')
+<div class="container height-fixer">
+    <div class="page-header mb-3">
+        <a href="{{ route('admin.rooms') }}" class="btn btn-secondary btn-sm mr-md-2">
+            <i class="fa fa-arrow-left"></i> Back
+        </a>
+        <span class="h5"><b>Agenda:</b> {{ $room->name }}</span>
+    </div>
+    <div class="card">
+        <div class="card-body">
+            {!! $calendar->calendar() !!}
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('scripts')
+    <script src="{{ asset('js/jquery.2.1.3.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('css/fullcalendar.min.css') }}"/>
+
+    {!! $calendar->script() !!}
+@stop
